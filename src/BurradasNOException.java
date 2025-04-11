25508553f46cd6845ff0b22b3bee2d49e4b6d85e
Nuria Moreno <uriamormag@gmail.com>
2025-04-11 16:02:07 +0200

    Clases con JavaDoc incluído

--- src/BurradasNOException.java
+++ src/BurradasNOException.java
@@ -1,14 +1,28 @@
+/**
+ * Excepción personalizada para manejar el lenguaje obsceno en mensajes.
+ * Esta clase hereda de la clase Exception.
+ */
+
 public class BurradasNOException extends Exception {
 	
+	/** La palabra prohibida que causó la excepción. */
     private String palabraProhibida;
 
-    //Constructor
+    /**
+     * Constructor para crear una nueva excepción con la palabra prohibida.
+     * 
+     * @param palabra La palabra prohibida que causó la excepción.
+     */
     public BurradasNOException(String palabra) {
         super("Error no se permite lenguaje obsceno."); //super() llama al constructor de la clase Exception con el mensaje de error
         this.palabraProhibida = palabra;
     }
 
-    // Getter para acceder a la palabra que causó el error
+    /**
+     * Obtiene la palabra prohibida que causó la excepción.
+     * 
+     * @return La palabra prohibida.
+     */
     public String getPalabraProhibida() {
         return palabraProhibida;
     }
--- src/Test.java
+++ src/Test.java
@@ -1,9 +1,18 @@
 import java.util.Scanner;
 
+/**
+ * Programa principal que simula la validación de mensajes para evitar lenguaje obsceno
+ * El programa lee mensajes del usuario y verifica si contienen palabras prohibidas
+ */
 public class Test {
 	
+	/** Array con una lista de palabras prohibidas. */
     static String tpalabros[] = {"caca","culo","pedo","pis"};
     
+    /**
+     * Método principal que gestiona el flujo del programa
+     * Permite al usuario introducir mensajes y verifica si contienen palabras prohibidas
+     */
     public static void main ( String argv[]){
         
         Scanner sc= new Scanner(System.in);
@@ -36,7 +45,13 @@ public class Test {
         
     }
      
-    // Método que lee una línea y verifica si contiene palabras prohibidas
+    /**
+     * Método que lee una línea del usuario y verifica si contiene palabras prohibidas
+     * 
+     * @param sc para leer las líneas del usuario.
+     * @return La línea que el usuario introdujo, si no contiene palabras prohibidas
+     * @throws BurradasNOException si la línea contiene una palabra prohibida
+     */
     public static String leerLineaOK (Scanner sc) throws BurradasNOException {
     	String linea = sc.nextLine();
     	
